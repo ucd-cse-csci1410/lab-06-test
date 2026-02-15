6fa0ed8a7d84730adbcf4b3b9ac80635c7e304c6
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2026-02-14 20:36:19 -0700

    typo

--- tests/test_02_package_discount_part_2.py
+++ tests/test_02_package_discount_part_2.py
@@ -18,7 +18,7 @@ import sys, os
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-from srcmain import package_discount_part_2
+from src.main import package_discount_part_2
 
 class TestPackageDiscountPart2(unittest.TestCase):
     """Test package_discount_part_2() by mocking input() and capturing printed output."""
